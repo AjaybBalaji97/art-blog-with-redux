8e33b1da3d3f935ed1adf924367d0e7667c3bbb1
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-02-12 10:47:56 +0530

    Few CSS changes

--- src/components/BlogHeader/index.js
+++ src/components/BlogHeader/index.js
@@ -4,16 +4,20 @@ import './BlogHeader.css';
 class BlogHeader extends Component {
   render() {
     return (
+      <header>
       <div className="header">
+      <div className="header-left">
       <span className="artifactClass">The Artifact</span>
       <br/>
       <span className="CultureClass">Culture & Art blog</span>
+      </div>
       <div className="header-right">
       <a  href="">Blog</a>
       <a href="">About</a>
       <a href="">Contact</a>
       </div>
       </div>
+      </header>
     )
   }
 }
