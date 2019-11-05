7f4dc1673f7ad94138479588351e1e5552eb1160
Ajay-B-Balaji <Ajay_B_Balaji@external.mckinsey.com>
2019-11-05 11:42:09 +0530

    Updates

---
+++ precache-manifest.df4b1e465ab434f4df431a806915cb4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9795b9dc503c46fb1335",
+    "url": "/art-blog-with-redux/static/js/runtime~main.9795b9dc.js"
+  },
+  {
+    "revision": "d0546565c66808ff66e9",
+    "url": "/art-blog-with-redux/static/js/main.d0546565.chunk.js"
+  },
+  {
+    "revision": "48ef98afca84b417b116",
+    "url": "/art-blog-with-redux/static/js/2.48ef98af.chunk.js"
+  },
+  {
+    "revision": "d0546565c66808ff66e9",
+    "url": "/art-blog-with-redux/static/css/main.cbe27309.chunk.css"
+  },
+  {
+    "revision": "c1e089c0aaa0a67306157110cf2f2cce",
+    "url": "/art-blog-with-redux/index.html"
+  }
+];
